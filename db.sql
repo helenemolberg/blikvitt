a4e7ed2b0cc52a588e93424f4e7e3bfbbb555abd
Nora Brask <nora.brask@teknologiporten.no>
2019-03-05 09:49:34 +0100

    La inn tabeller i database

--- db.sql
+++ db.sql
@@ -1,9 +1,23 @@
-CREATE TABLE pant (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE Station(
+  stationID SERIAL PRIMARY KEY,
+  type VAR (F,M,P),
+  place VARCHAR (50),
+  lat DOUBLE DEFAULT 0,
+  lng DOUBLE DEFAULT 0
+);
+
+
+CREATE TABLE User (
+  userID SERIAL PRIMARY KEY,
+  name VARCHAR (30),
+  password VARCHAR (30),
   value DECIMAL(4, 2)
 );
 
-CREATE TABLE user (
+
+CREATE TABLE report(
   id SERIAL PRIMARY KEY,
-  value DECIMAL(4, 2)
-);
+  fk_userID int FOREIGN KEY REFERENCES User(userID),
+  fk_stationID int FOREIGN KEY REFERENCES Station(stationID),
+  description VARCHAR (100)
+)
