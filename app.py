7c56f18411ce3031cff45b45db81256af00ea0cf
Helene Molberg <helenemolberg@gmail.com>
2019-03-22 10:32:08 +0100

    deleted unused files

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from flask_migrate import Migrate, MigrateCommand
 
 import config
 
+'''Database connection'''
 application = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URI
 
+'''Using SQLAlchemy and Migrate'''
 db = SQLAlchemy(application)
 migrate = Migrate(application, db)
 
@@ -16,13 +18,46 @@ manager = Manager(application)
 manager.add_command('db', MigrateCommand)
 
 
+'''Creating tables for the database'''
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(80))
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class RecycleData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    postalcode = db.Column(db.Integer, nullable=False)
+    lat = db.Column(db.DECIMAL, nullable=False)
+    long = db.Column(db.DECIMAL, nullable=False)
+    extraInfo = db.Column(db.String(80))
+
+
+class FretexData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    postalcode = db.Column(db.Integer, nullable=False)
+    lat = db.Column(db.DECIMAL, nullable=False)
+    long = db.Column(db.DECIMAL, nullable=False)
+    extraInfo = db.Column(db.String(80))
+
+
+class PantData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    postalcode = db.Column(db.Integer, nullable=False)
+    lat = db.Column(db.DECIMAL, nullable=False)
+    long = db.Column(db.DECIMAL, nullable=False)
+    extraInfo = db.Column(db.String(80))
+
+
+'''Rendering html'''
 
 
 @application.route('/')
@@ -45,7 +80,7 @@ def recycle():
     return render_template('recycle.html')
 
 
-@application.route('/login')
+@application.route('/login', methods=('get', 'post'))
 def login():
     return render_template('userLogin.html')
 
@@ -64,23 +99,8 @@ def registeruser():
         return render_template('registerUser.html', message='Please fill in this form to create an account.')
 
 
-''''@application.route('/add_pant', methods=['GET', 'POST'])
-def add_pant():
-    with database_connection.cursor() as cursor:
-        if request.method == 'POST':
-            pant = request.form['pant']
-            print(pant)
-            cursor.execute("""
-                insert into pant (value)
-                values (%s)
-            """, (pant,))
-        database_connection.commit()
-        cursor.execute('select * from pant')
-        pant = cursor.fetchall()
-    return render_template('add_pant.html', pant=pant)
-'''
-
 if __name__ == '__main__':
     manager.run()
+
 
 '''to create a new table use db.create_all()'''
